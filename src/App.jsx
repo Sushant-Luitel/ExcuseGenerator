f3f3c5a3de13eb4c5592b3fcf2529a1d17f89e44
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-25 20:47:29 +0545

    added Gaming excuse

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,11 @@ function App() {
     <>
       <h1>Excuse Generator</h1>
       <h2>Pick a Category you like to generate the Excuse for:</h2>
+
       <button onClick={() => fetchAge("family")}>Family</button>
       <button onClick={() => fetchAge("funny")}>Funny</button>
       <button onClick={() => fetchAge("party")}>Party</button>
+      <button onClick={() => fetchAge("gaming")}>Gaming</button>
       <div
         style={{
           margin: "3rem auto",
